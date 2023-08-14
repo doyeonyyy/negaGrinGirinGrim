73c5ea8ad3b34a15494199b7d0e03315e0a31225
churoo <churoo0418@gmail.com>
2023-08-14 17:02:45 +0900

    컬렉션뷰에 셀 띄우기 성공!

--- nagaGrinGirinGrim/nagaGrinGirinGrim/MainPageViewController.swift
+++ nagaGrinGirinGrim/nagaGrinGirinGrim/MainPageViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class MainPageViewController: UIViewController {
 
+    
+    
+    
+    
+    
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
+        
+    }
+}
+
+extension MainPageViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mainCollectionCell", for: indexPath) as?
+                   UICollectionViewCell else {
+               return UICollectionViewCell()
+           }
+           return cell
     }
-    */
-
 }
