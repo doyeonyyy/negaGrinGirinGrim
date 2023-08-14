d83dfd5ed2c8f70f16d7fb6b8f6d345aea1383e0
Daisy Kim <doyeonyyy@gmail.com>
2023-08-14 14:32:06 +0900

    first commit

---
+++ nagaGrinGirinGrim/nagaGrinGirinGrim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nagaGrinGirinGrim
+//
+//  Created by t2023-m0047 on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
