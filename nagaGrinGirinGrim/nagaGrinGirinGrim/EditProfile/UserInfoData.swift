a88f7d57231baa1fa9e0f0925fbd94a99c44f571
kiakim01 <hnmpot0318@naver.com>
2023-08-17 16:25:56 +0900

    MockData 적용

---
+++ nagaGrinGirinGrim/nagaGrinGirinGrim/EditProfile/UserInfoData.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+
+
+struct UserInfo {
+    var profilePicture: String?
+    var name: String
+    var mood: String?
+    var introduction : String?
+    var anniversaryTitle : String?
+    var anniversary : String?
+}
+
+
+let userInfoData = UserInfo(
+    profilePicture: "https://ca.slack-edge.com/T043597JK8V-U05D6G5MADS-484937bd3678-512",
+    name: "kia kim",
+    mood: "Help me",
+    introduction: "I love my Job!",
+    anniversaryTitle: "생일",
+    anniversary: "September 1st"
+)
+
