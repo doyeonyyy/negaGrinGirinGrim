0adb61d903ebc372fa2854e0890e5e91ad35262f
Nam Bokyeong <bo_bo_@kakao.com>
2023-08-18 10:01:56 +0900

    merge branches dev andpost_doyeon together

---
+++ negaGrinGirinGrim/nagaGrinGirinGrim/nagaGrinGirinGrim/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  nagaGrinGirinGrim
+//
+//  Created by 보경 on 2023/08/18.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
